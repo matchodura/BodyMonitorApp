79b7daf139d0219e3ab323f8b7f95357d515eebe
matchodura <69363257+matchodura@users.noreply.github.com>
2021-02-22 18:18:13 +0100

    Delete DatabaseBodyMonitor/dbo/Tables directory

--- DatabaseBodyMonitor/dbo/Tables/Users.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Users] (
-    [UserId]       INT            IDENTITY (1, 1) NOT NULL,
-    [UserLogin]    NCHAR (10)     NULL,  
-    [Hash]         NVARCHAR (MAX) NULL,
-    [Salt]         NVARCHAR (MAX) NULL,
-    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([UserId] ASC),
-    UNIQUE NONCLUSTERED ([UserLogin] ASC)
-);
-
